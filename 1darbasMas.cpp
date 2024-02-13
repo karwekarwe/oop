34468f14726adde5b55311597a6c3e74d058abfd
Dominykas Mykolaitis <domasmyko@gmail,com>
2024-02-14 00:39:57 +0200

    ivesties apsaugos abiejose versijose

--- 1darbasMas.cpp
+++ 1darbasMas.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <string>
 #include <algorithm>
+#include <limits>
 
 using namespace std;
 
@@ -20,61 +21,115 @@ double mediana(Stud&);
 int main() {
     int m = 0; // studentu sk
     int n = 0; // namu darbu sk
-    int balas;
+    int balas, egzBalas;
     char pabaiga;
+    
 
     Stud *studentai = new Stud[100];
     int *tempND = new int[100];
 
     while (true) {
-        cout << "Vardas: ";
+        cout << "Vardas: "<< endl;
         cin >> studentai[m].vardas; //vardas i struct
-        cout << "Pavarde: ";
+        cout << "Pavarde: "<< endl;
         cin >> studentai[m].pavarde; //pavarde i struct
-
-        cout << "Namu darbu balai (-1 norint baigti): ";
-        while (true) {
+        int i = 1;
+        do{
+           
+            cout << "Namu darbu "<< i <<" balas (-1 norint baigti): "<< endl;
             cin >> balas;
-            if (balas == -1){
-            break;
+
+             if (cin.fail() || balas == 0 || balas < -1 || balas > 10) {
+                cout << "Įvekite skaičių nuo 1 iki 10" << endl;
+                cin.clear(); 
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+                continue; 
             }
+            if (balas == -1){
+                if (n == 0) {
+                    cout << "Įveskite bent 1 pažymį: " << endl;
+                    continue; 
+                }
+                else {
+                    break; 
+                }
+            }
+
+               
             tempND[n++] = balas;
+            studentai[m].namuDarbai = tempND; //nd array i struct
+            studentai[m].ndCount = n;
+            i++;
+        } while (true);
+
+        do {
+            cout << "Iveskite egzamino bala: ";
+                        cin >> egzBalas;
+
+            if (cin.fail() || egzBalas < 1 || egzBalas > 10) {
+                cout << "Įvekite skaičių nuo 1 iki 10" << endl;
+                cin.clear(); 
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+                continue; 
+            }
+            else {
+                studentai[m].egzaminas = egzBalas; // egzaminas i struct
+                break;
+            }      
+  //          cin >> studentai[m].egzaminas; //egzaminas i struct
+
+        } while (true); 
+
+            m++;
+
+            while (true) {
+                cout << "Ar baigete ivesti studentus? (y/n): ";
+                cin >> pabaiga;
+                if (pabaiga == 'y' || pabaiga == 'Y') {
+                   break;
+                } 
+                else if (pabaiga == 'n' || pabaiga == 'N') {
+                    break;
+                } 
+                else {
+                    cout << "Įveskite 'n' arba 'y'" << endl;
+                }
+            } 
+   
+            if (pabaiga == 'y' || pabaiga == 'Y') {
+                break;
         }
-        studentai[m].namuDarbai = tempND; //nd array i struct
-        studentai[m].ndCount = n;
-
-        cout << "Iveskite egzamino bala: ";
-        cin >> studentai[m].egzaminas; //egzaminas i struct
-
-        m++;
-        cout << "Ar baigete ivesti studentus? (y/n): ";
-        cin >> pabaiga;
-        if (pabaiga == 'y' || pabaiga == 'Y'){
-            break;
-        }      
+           
     }
 
     char pasirinkimas;
-    cout << "Pasirinkite skaiciavimo metoda (V - vidurkis, M - mediana): ";
-    cin >> pasirinkimas;
 
-     if (pasirinkimas == 'V' || pasirinkimas == 'v') {
-        cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << right << "Galutinis (Vid.)" << endl;
-         cout << "--------------------------------------------" << endl;
-        for (int i = 0; i < m; ++i) {
-            studentai[i].galutinis = vidurkis(studentai[i]); // galutinis i struct
-            cout << setw(20) << left << studentai[i].vardas << setw(20) << left << studentai[i].pavarde << setw(10) << right << fixed << setprecision(2) << studentai[i].galutinis << endl;
+        while (true) {
+            cout << "Pasirinkite skaiciavimo metoda (V - vidurkis, M - mediana): ";
+            cin >> pasirinkimas;
+
+            if (pasirinkimas == 'V' || pasirinkimas == 'v') {
+                cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << right << "Galutinis (Vid.)" << endl;
+                cout << "--------------------------------------------" << endl;
+                for (int i = 0; i < m; ++i) {
+                    studentai[i].galutinis = vidurkis(studentai[i]); // galutinis i struct
+                    cout << setw(20) << left << studentai[i].vardas << setw(20) << left << studentai[i].pavarde << setw(10) << right << fixed << setprecision(2) << studentai[i].galutinis << endl;
+                break;
+                }
+            } 
+            else if (pasirinkimas == 'M' || pasirinkimas == 'm') {
+                cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << right << "Galutinis (Med.)" << endl;
+                cout << "--------------------------------------------" << endl;
+                for (int i = 0; i < m; ++i) {
+                    studentai[i].galutinis = mediana(studentai[i]); // galutinis i struct
+                    cout << setw(20) << left << studentai[i].vardas << setw(20) << left << studentai[i].pavarde << setw(10) << right << fixed << setprecision(2) << studentai[i].galutinis << endl;
+                break;
+                }
+            } 
+            else {
+                cout << "Neteisinga įvestis." << endl;
+            }
         }
-    } else if (pasirinkimas == 'M' || pasirinkimas == 'm') {
-        cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << right << "Galutinis (Med.)" << endl;
-         cout << "--------------------------------------------" << endl;
-        for (int i = 0; i < m; ++i) {
-            studentai[i].galutinis = mediana(studentai[i]); // galutinis i struct
-            cout << setw(20) << left << studentai[i].vardas << setw(20) << left << studentai[i].pavarde << setw(10) << right << fixed << setprecision(2) << studentai[i].galutinis << endl;
-        }
-    } else {
-        cout << "Neteisingas pasirinkimas." << endl;
-    }
 
     delete[] tempND;
     delete[] studentai;
--- 1darbasVector.cpp
+++ 1darbasVector.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <limits>
 
 using namespace std;
 
@@ -19,7 +20,7 @@ double mediana(Stud&);
 
 int main() {
     int m = 0; // studentu sk
-    int balas;
+    int balas, egzBalas;
     char pabaiga;
 
     vector<Stud> studentai;
@@ -29,55 +30,102 @@ int main() {
 
         Stud naujasS;
 
-        cout << "Vardas: ";
+        cout << "Vardas: "<< endl;
         cin >> naujasS.vardas; //vardas i struct
-        cout << "Pavarde: ";
+        cout << "Pavarde: "<< endl;
         cin >> naujasS.pavarde; //pavarde i struct
 
-        cout << "Namu darbu balai (-1 norint baigti): ";
-        while (true) {
+        int i = 1;
+        do {
+            cout << "Namu darbu "<< i <<" balas (-1 norint baigti): " << endl;
             cin >> balas;
-            if (balas == -1){
-                break;
-            }
-            naujasS.namuDarbai.push_back(balas);
-        }
 
-        cout << "Iveskite egzamino bala: ";
-        cin >> naujasS.egzaminas; //egzaminas i struct
+            if (cin.fail() || balas == 0 || balas < -1 || balas > 10) {
+                cout << "Įvekite skaičių nuo 1 iki 10" << endl;
+                cin.clear(); 
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+                continue; 
+            }
+            if (balas == -1){
+                if (naujasS.namuDarbai.empty()) {
+                    cout << "Įveskite bent 1 pažymį: " << endl;
+                    continue; 
+                } 
+                else {
+                    break; 
+                }
+                }
+        
+            naujasS.namuDarbai.push_back(balas); //namu darbu balai i struct
+            i++;
+        } while (true);
+
+        do {
+            cout << "Iveskite egzamino bala: ";
+            cin >> egzBalas;
+
+            if (cin.fail() || egzBalas < 1 || egzBalas > 10) {
+                cout << "Įvekite skaičių nuo 1 iki 10" << endl;
+                cin.clear(); 
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+                continue; 
+            }
+            else {
+                naujasS.egzaminas = egzBalas; // egzaminas i struct
+                break;
+            }      
+        } while (true);
+
 
         studentai.push_back(naujasS); 
 
+        while (true) {
+            cout << "Ar baigete ivesti studentus? (y/n): ";
+            cin >> pabaiga;
+            if (pabaiga == 'y' || pabaiga == 'Y') {
+                break;
+            } 
+            else if (pabaiga == 'n' || pabaiga == 'N') {
+                break;
+            } 
+            else {
+                cout << "Įveskite 'n' arba 'y'" << endl;
+            }
+        }
 
-        cout << "Ar baigete ivesti studentus? (y/n): ";
-        cin >> pabaiga;
-        if (pabaiga == 'y' || pabaiga == 'Y'){
+        if (pabaiga == 'y' || pabaiga == 'Y') {
             break;
-        }      
+        }
     }
-
+    
     char pasirinkimas;
-    cout << "Pasirinkite skaiciavimo metoda (V - vidurkis, M - mediana): ";
-    cin >> pasirinkimas;
 
-     if (pasirinkimas == 'V' || pasirinkimas == 'v') {
-        cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << right << "Galutinis (Vid.)" << endl;
-         cout << "--------------------------------------------" << endl;
-        for (auto& studentas : studentai) {
-            studentas.galutinis = vidurkis(studentas); // galutinis i struct
-            cout << setw(20) << left << studentas.vardas << setw(20) << left << studentas.pavarde << setw(10) << right << fixed << setprecision(2) << studentas.galutinis << endl;
+    while (true) {
+        cout << "Pasirinkite skaiciavimo metoda (V - vidurkis, M - mediana): ";
+        cin >> pasirinkimas;
+
+        if (pasirinkimas == 'V' || pasirinkimas == 'v') {
+            cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << right << "Galutinis (Vid.)" << endl;
+            cout << "--------------------------------------------" << endl;
+            for (auto& studentas : studentai) {
+                studentas.galutinis = vidurkis(studentas); // galutinis i struct
+                cout << setw(20) << left << studentas.vardas << setw(20) << left << studentas.pavarde << setw(10) << right << fixed << setprecision(2) << studentas.galutinis << endl;
+            }
+            break;
+        } 
+        else if (pasirinkimas == 'M' || pasirinkimas == 'm') {
+            cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << right << "Galutinis (Med.)" << endl;
+            cout << "--------------------------------------------" << endl;
+            for (auto& studentas : studentai) {
+                studentas.galutinis = mediana(studentas); // galutinis i struct
+                cout << setw(20) << left << studentas.vardas << setw(20) << left << studentas.pavarde << setw(10) << right << fixed << setprecision(2) << studentas.galutinis << endl;
+            }
+            break;
+        } 
+        else {
+            cout << "Neteisinga įvestis." << endl;
         }
-    } else if (pasirinkimas == 'M' || pasirinkimas == 'm') {
-        cout << setw(20) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(10) << right << "Galutinis (Med.)" << endl;
-         cout << "--------------------------------------------" << endl;
-        for (auto& studentas : studentai) {
-            studentas.galutinis = mediana(studentas); // galutinis i struct
-            cout << setw(20) << left << studentas.vardas << setw(20) << left << studentas.pavarde << setw(10) << right << fixed << setprecision(2) << studentas.galutinis << endl;
-        }
-    } else {
-        cout << "Neteisingas pasirinkimas." << endl;
     }
-
     return 0;
 
 }
