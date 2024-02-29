4aa9239517ad861d47c0b81f522b4bd6d6f4a27c
Dominykas Mykolaitis <domasmyko@gmail,com>
2024-03-01 00:43:17 +0200

    rusiavimas.cpp

---
+++ rusiavimas.cpp
@@ -0,0 +1,24 @@
+
+#include "funkcijos.h"
+#include <iostream>
+
+using namespace std;
+
+bool rusiavimas(const Stud& a, const Stud& b, char metPas) {
+
+    metPas = toupper(metPas);
+
+    if (metPas == 'V') {
+        return a.vardas < b.vardas;
+    }
+    else if (metPas == 'P') {
+        return a.pavarde < b.pavarde;
+    }
+    else if (metPas == 'G') {
+        return a.galutinis < b.galutinis;
+    }
+    else {
+        cout << "Neteisinga įvestis." << endl;
+        return false;
+    }
+}
