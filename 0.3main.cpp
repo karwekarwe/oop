112663a8d547e69c6ec0346ce5cca5076231384f
Dominykas Mykolaitis <domasmyko@gmail,com>
2024-03-01 00:47:48 +0200

    mediana.cpp

--- 0.3main.cpp
+++ 0.3main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 
 #include "student.h"
 #include "funkcijos.h"
@@ -29,17 +29,3 @@ int main() {
 
     return 0;
 }
-    double mediana (Stud& s) {
-        double med = 0;
-        double gal;
-        sort(s.namuDarbai.begin(), s.namuDarbai.end());
-        if (s.namuDarbai.size() % 2 == 0) {
-            med = (s.namuDarbai[s.namuDarbai.size() / 2 - 1] + s.namuDarbai[s.namuDarbai.size() / 2]) / 2.0;
-        }
-        else {
-            med = s.namuDarbai[s.namuDarbai.size() / 2];
-        }
-        gal = med*0.4 + s.egzaminas*0.6;
-
-        return gal;
-    }
