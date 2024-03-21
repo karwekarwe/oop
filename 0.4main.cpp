8ea14921ce594048cbea385d3c30a813b80d5c06
Dominykas Mykolaitis <domasmyko@gmail,com>
2024-03-21 21:50:16 +0200

    pasirinkimas dirbti su jau sugenereotais failais

--- 0.4main.cpp
+++ 0.4main.cpp
@@ -18,41 +18,72 @@ int main() {
 
 
         try {
-               
+                char ne;
+                cout << "Generuoti nauja ar naudoti esama? (Nauja - N. Esama - E)" << endl;
+                cin >> ne;
+
                 char pasDyd;
-                cout << "Pasirinkite, kokio dydzio faila norite generuoti: 1. 1000 2. 10000 3. 100000 4. 1000000 5. 10000000"<< endl;
+                cout << "Pasirinkite, su kokio dydzio failu norite dirbti: 1. 1000 2. 10000 3. 100000 4. 1000000 5. 10000000"<< endl;
                 cin >> pasDyd;
 
-                switch (pasDyd) {
-                    case '1':
-                    failuGen("studentai1000.txt", 1000);
-                    failPav = "studentai1000.txt";
-                    break;
-                    case '2':
-                    failuGen("studentai10000.txt", 10000);
-                    failPav = "studentai10000.txt";
-                    break;
-                    case '3':
-                    failuGen("studentai100000.txt", 100000);
-                    failPav = "studentai100000.txt";
-                    break;
-                    case '4':
-                    failuGen("studentai1000000.txt", 1000000);
-                    failPav = "studentai1000000.txt";
-                    break;
-                    case '5':
-                    failuGen("studentai10000000.txt", 10000000);
-                    failPav = "studentai10000000.txt";
-                    break;
-                    default:
-                    throw invalid_argument("Klaida");
+                    
+                if (ne == 'N' || ne == 'n') {
+
+
+                    switch (pasDyd) {
+                        case '1':
+                        failuGen("studentai1000.txt", 1000);
+                        failPav = "studentai1000.txt";
+                        break;
+                        case '2':
+                        failuGen("studentai10000.txt", 10000);
+                        failPav = "studentai10000.txt";
+                        break;
+                        case '3':
+                        failuGen("studentai100000.txt", 100000);
+                        failPav = "studentai100000.txt";
+                        break;
+                        case '4':
+                        failuGen("studentai1000000.txt", 1000000);
+                        failPav = "studentai1000000.txt";
+                        break;
+                        case '5':
+                        failuGen("studentai10000000.txt", 10000000);
+                        failPav = "studentai10000000.txt";
+                        break;
+                        default:
+                        throw invalid_argument("Klaida");
+
+                    }
+                    cout << "Failas sugeneruotas." << endl;
+  
 
                 }
-                cout << "Failas sugeneruotas." << endl;
-                isFailo(failPav, studentai); 
-                        
+                else {
 
-                rusiavimasGen(failPav, studentai); 
+                    switch (pasDyd) {
+                        case '1':
+                        failPav = "studentai1000.txt";
+                        break;
+                        case '2':
+                        failPav = "studentai10000.txt";
+                        break;
+                        case '3':
+                        failPav = "studentai100000.txt";
+                        break;
+                        case '4':
+                        failPav = "studentai1000000.txt";
+                        break;
+                        case '5':
+                        failPav = "studentai10000000.txt";
+                        break;
+                        default:
+                        throw invalid_argument("Klaida");
+                    }
+
+                    isFailo(failPav, studentai); 
+                    rusiavimasGen(failPav, studentai); 
+                }
             
 
 
@@ -62,6 +93,8 @@ int main() {
             cout << "Neatpažinta klaida: " << e.what() << " Programa baigia darba.\n";
             return 1;
         }
+
+
 
     cout << "Visos programos veikimo laikas: " << totalTime.count() << " milisekundes" << endl;
 
--- README.md
+++ README.md
@@ -2,22 +2,22 @@
 
 ## Apie:
 
-# v0.3
+# v0.4
 
-Ši C++ programa skaičiuoja studentų galutinius pažymius pagal jų namų darbų ir egzaminų rezultatus. Ji leidžia vartotojui įvesti studentų skaičių, namų darbų užduočių skaičių ir namų darbų pažymius ir pasirinkti, ar galutinis pažymys bus skaičiuojamas naudojant namų darbų vidurkį arba medianą. v0.2 pridėta galimybė pasirinkti ar duomenis vesti ranka, ar nuskaityti iš failo.
+Failų su pasirinktu dydžiu duomenų generavimas, skaitymas, rūšiavimas.
 
-Ši programos versija suskirstyta į antraštinius failus
+Testavimas: 
+(2 tyrimas) 
 
-## Funkcionalumas:
+Atlikti 4 testai su kiekvienu failu. 
 
-1. Paklausiama, ar vartotojas nori vest duomenis ranka, ar nuskaityi iš failo.
-1. Programa paprašo vartotojo įvesti studentų skaičių.
-2. Vvartotojas prašomas įvesti namų darbų užduočių skaičių.
-3. Vartotojas pasirenka, ar galutinis pažymys bus skaičiuojamas naudojant namų darbų vidurkį ar medianą.
-4. Kiekvienam studentui programa paprašo įvesti jų vardą, pavardę, namų darbų pažymius ir egzamino rezultatą.
-5. Įvedus visus reikiamus duomenis, programa skaičiuoja ir rodo kiekvieno studento galutinius pažymius.
+Skaičiuojami laikai: Failo skaitymas, rūšiavimas (<5 ir >=5), surūšiuotų studentų išvedimas į atskirus failus (luzeriukai ir intelektualai). 
+Šie laikai sudedami į visos programos veikimo laiką.
 
-## Pastabos:
 
-1. Programa patikrina vartotojo įvestis, kad jos būtų nurodytuose diapazonuose ir formatuose.
-2. Galutinis pažymys skaičiuojamas naudojant formulę: 0.4 * (namų darbų vidurkis / mediana) + 0.6 * (egzamino rezultatas).
+Vidurkiai(milisekundėmis):
+1000 - 1062.5
+10000 - 1008
+100000 - 2532.25
+1000000 - 8359.75
+10000000 - 94962
